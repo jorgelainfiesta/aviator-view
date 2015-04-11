40fa898db8a77e74d56f67be12ee182f50a626dc
Jorge Lainfiesta <jorge.lainfiesta@gmail.com>
2015-04-11 14:34:34 -0600

    Added minimalistic front server. Added dummy sockets for testing. Cleaned variables.

---
+++ web/dummy_socket.py
@@ -0,0 +1,26 @@
+import tornado.httpserver
+import tornado.websocket
+import tornado.ioloop
+import tornado.web
+
+class WSHandler(tornado.websocket.WebSocketHandler):
+
+  def open(self):
+    print('user is connected.\n')
+
+  def on_message(self, message):
+    print('received message: %s\n' % message)
+    self.write_message(message + ' OK')
+
+  def on_close(self):
+    print('connection closed\n')
+    
+  def check_origin(self, origin):
+    return True
+
+application = tornado.web.Application([(r'/ws', WSHandler),])
+
+if __name__ == "__main__":
+  http_server = tornado.httpserver.HTTPServer(application)
+  http_server.listen(9999)
+  tornado.ioloop.IOLoop.instance().start()
