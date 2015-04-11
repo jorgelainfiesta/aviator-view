65f355ed8e9244e0d2fbb5f7c88bc6679f81efd6
Eddy Castro <cas11032@uvg.edu.gt>
2015-04-11 16:49:45 -0600

    Solving problems

--- web/scripts/app/opts.js
+++ web/scripts/app/opts.js
@@ -3,7 +3,6 @@ define({
 	sheight: window.innerHeight,
 	near: 1,
 	far: 6000,
-    socketurl: 'ws://localhost:9999/ws'
-	far: 6000,
+  socketurl: 'ws://localhost:9999/ws',
 	cloudURL: '././images/cloud.png',
 });
